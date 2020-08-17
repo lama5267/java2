00c164269fe39dc6e61f0ef15f6222feff415e41
Maks <lama526@yandex.ru>
2020-08-17 21:42:37 +0200

    dz6

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -4,8 +4,22 @@ public class Binary {
     byte num;
     public Binary(byte num){
         this.num = num;
+
     }
-    public String  toString(){
-        String s = "I am" + " " ;
-return s;    }
+        @Override
+        public String toString() {
+            String result = "";
+            result += (num >> 7) & 0b00000001;
+            result += (num >> 6) & 0b00000001;
+            result += (num >> 5) & 0b00000001;
+            result += (num >> 4) & 0b00000001;
+            result += (num >> 3) & 0b00000001;
+            result += (num >> 2) & 0b00000001;
+            result += (num >> 1) & 0b00000001;
+            result += (num >> 0) & 0b00000001;
+            return result;
+        }
+
+    public static void main(String[] args) {
     }
+}
