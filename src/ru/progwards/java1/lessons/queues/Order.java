0d0199a0d4a6bc98705f198374c9e40c0dbbe318
Maks <lama526@yandex.ru>
2020-09-21 21:53:17 +0200

    dz14

--- src/ru/progwards/java1/lessons/queues/Order.java
+++ src/ru/progwards/java1/lessons/queues/Order.java
@@ -1,16 +1,33 @@
 package ru.progwards.java1.lessons.queues;
 
 public class Order {
-private double sum;
-    private int num;
+    private double sum;
+    private int num = 0;
+    static int nnum = 0;
 
     public Order(double sum) {
         this.sum = sum;
+        num = numm();
     }
-    public double getSum(){
+
+    public double getSum() {
         return sum;
     }
-    public int getNum(){
+
+    public int getNum() {
         return num;
     }
+
+    static int numm() {
+        return ++nnum;
+    }
+
+    public static void main(String[] args) {
+        Order o1 = new Order(10);
+        Order o2 = new Order(10);
+        Order o3 = new Order(10);
+
+        System.out.println(o1.getNum() + "   " + o2.getNum() + "    " + o3.getNum());
+    }
+
 }
